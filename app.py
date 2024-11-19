38d61a268d55f734ab4cc4d4cd59ae54576f2ac9
Han Ji Seok <busker362@gmail.com>
2024-11-20 01:08:17 +0900

    스포티파이 api를 연동
    스포티파이 api를 연동해서 사용자 정보를 불러오는 login() 과 callback() 구현 하였습니다.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, session
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -9,7 +9,8 @@ with open("config.json") as f:
 
 CLIENT_ID = config["client_id"]
 CLIENT_SECRET = config["client_secret"]
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+REDIRECT_URI = "http://127.0.0.1:5000/api/callback"
+SECRET_KEY = config["secret_key"]  
 
 # SpotifyOAuth 객체 생성
 sp_oauth = SpotifyOAuth(
@@ -21,16 +22,44 @@ sp_oauth = SpotifyOAuth(
 
 # Flask 애플리케이션 초기화
 app = Flask(__name__)
-    
-# @app.route("/")
-# def home():
-#     return render_template("home.html", user=None, playlists=[])
+app.secret_key = SECRET_KEY  
 
 @app.route("/")
 def home():
     message = "안녕하세요 시작해봅시다"
     return render_template("home.html", user=None, playlists=[], message=message)
 
+#스포티파이 API를 받아오기 위해서 연결해야함 
+@app.route("/login", methods=["GET"])
+def login():
+    auth_url = sp_oauth.get_authorize_url() #스포티파이 인증페이지 반환
+    return redirect(auth_url)
 
+#로그인 버튼을 누른 후 인증 코드를 받아올 메서드
+@app.route("/api/callback", methods=["GET"])
+def callback():
+    code = request.args.get("code")
+    token_info = sp_oauth.get_access_token(code)
+    access_token = token_info["access_token"]
+
+    session["token_info"] = token_info
+
+    #spotipy api객체 초기화 
+    sp = spotipy.Spotify(auth=access_token)
+    #user정보 가져오기 
+    user_info = sp.current_user()
+    user_name = user_info['display_name']
+    print(f'사용자 닉네임 {user_name}')
+
+    # 사용자 플레이리스트 가져오기
+    playlists = sp.current_user_playlists(limit=10)
+
+    # 플레이리스트 데이터 정리
+    playlist_data = [
+        {"name": playlist["name"], "id": playlist["id"]}
+        for playlist in playlists["items"]
+    ]
+    return render_template("home.html", user=user_info, playlists=playlist_data, message="안녕하세요 시작해봅시다")
+    
 if __name__ == "__main__":
     app.run(debug=True)
